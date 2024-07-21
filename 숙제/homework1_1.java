5f640b9c85280c7928fabb4c5c43c15146e1e960
rudwk <tlsruwk12345@gmail.com>
2024-07-22 03:10:29 +0900

    숙제

---
+++ 숙제/homework1_1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class homework1_1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int answer = x > 10 && x < 20 ? 1 : 0;
+        System.out.println(answer);
+
+        char ch = sc.next();
+
+    }
+}
